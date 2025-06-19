fee86adf5f8acbb862b6a46cde2763733c0430a6
rannicamp <rannierecampos1@gmail.com>
2025-06-19 10:52:00 -0300

    Corrige uso da função drizzle com schema e client

--- db/index.ts
+++ db/index.ts
@@ -3,4 +3,4 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from './schema';
 
-export const db = drizzle(neon(), { schema });
+export const db = drizzle([neon(), { schema }]);
